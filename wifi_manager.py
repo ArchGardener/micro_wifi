b84f129ba7adb599322c75d8b94c2ccfb37f49a0
DanBoothDev <danboothdev@gmail.com>
2021-02-07 16:13:16 -0400

    Updated connection method to wait for successful connection

--- wifi_manager.py
+++ wifi_manager.py
@@ -1,13 +1,15 @@
+import time
 import network
 
 
 class WifiManager:
 
-    def __init__(self, ap_name='EspAP', ap_password='', ap_max_clients=10, filepath=''):
+    def __init__(self, ap_name='EspAP', ap_password='', ap_max_clients=10, filepath='', connection_max_retries=10):
         self._filepath = filepath
         self._ap_name = ap_name
         self._ap_password = ap_password
         self._ap_max_clients = ap_max_clients
+        self._connection_max_retries = connection_max_retries
         self.wlan = network.WLAN(network.STA_IF)
         self.ap = network.WLAN(network.AP_IF)
         self._profiles = self._read_profiles()
@@ -23,6 +25,7 @@ class WifiManager:
         self.access_point_scan()
 
     def start_ap(self):
+        self.stop_wlan()
         # activate the interface
         self.ap.active(True)
         # configure the params 
@@ -34,22 +37,35 @@ class WifiManager:
 
     def start_wlan(self):
         # activate the interface
+        self.stop_ap()
         self.wlan.active(True)
+
+    def stop_wlan(self):
+        # deactivate the interface
+        self.wlan.active(False)
 
     def connect(self, essid='', password=''):
         if self.wlan.isconnected():
             self.disconnect()
-        print('connecting to {}'.format(essid))
+        print('connecting to {} . . .'.format(essid))
+        # switch from ap to wlan mode
         self.start_wlan()
         self.wlan.connect(essid, password)
-        if self.wlan.isconnected():
-            print('connected')
-            self._add_new_profile(essid, password)
-            self.stop_ap()
-            return True
-        print('error connecting to {}'.format(essid))
-        return False
-    
+        # wait a few seconds to see if the connection was successful
+        for _ in range(self._connection_max_retries * 2):
+            if self.wlan.isconnected():
+                break
+            time.sleep(0.5)
+            print('. . .')
+        if not self.wlan.isconnected():
+            print('error connecting to {}'.format(essid))
+            # enable ap again
+            self.start_ap()
+            return False
+        print('connected')
+        self._add_new_profile(essid, password)
+        return True
+
     def disconnect(self):
         print('disconnecting . . .')
         self.wlan.disconnect()
