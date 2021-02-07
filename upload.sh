3becda8c54ab20b9150d243496534649a5348b7f
DanBoothDev <danboothdev@gmail.com>
2021-02-07 14:36:06 -0400

    Added error checking to upload script

--- upload.sh
+++ upload.sh
@@ -4,8 +4,14 @@ export AMPY_PORT=/dev/ttyUSB0
 export AMPY_BAUD=115200
 
 for i in $(find -not -path "./venv/*" -name "*.py" -o -name "*.html"); do # Whitespace-safe but not recursive.
-    echo uploading "$i"  
+    echo Uploading "$i"  
     ampy put "$i" "$i"
+    if [ $? -eq 0 ]; then
+        echo ... OK
+    else
+        echo ... FAILED
+        exit
+    fi
 done
 
 echo rebooting
