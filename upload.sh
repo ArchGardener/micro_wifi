d27330bbdd04f1bd8f930f88f5caf9071165f170
Dan Booth <DanBoothDev@gmail.com>
2021-02-08 18:24:34 -0400

    Added path to upload script

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@
 export AMPY_PORT=/dev/ttyUSB0
 export AMPY_BAUD=115200
 
-for i in $(find -not -path "./venv/*" -name "*.py" -o -name "*.html"); do # Whitespace-safe but not recursive.
+for i in $(find . -not -path "./venv/*" -name "*.py" -o -name "*.html"); do # Whitespace-safe but not recursive.
     echo Uploading "$i"  
     ampy put "$i" "$i"
     if [ $? -eq 0 ]; then
